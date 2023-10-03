12bd04cd0bc96c7a4a28ce2487a4568841a6fca4
mouadlouhichi <mouadlouhichi@gmail.com>
2023-10-03 17:46:44 +0100

    ✅ fix: test setup

--- jest.setup.ts
+++
@@ -1,7 +0,0 @@
-/** @see https://jest-extended.jestcommunity.dev/docs/getting-started/setup */
-
-import "@testing-library/jest-dom";
-
-import * as matchers from "jest-extended";
-
-expect.extend(matchers);
