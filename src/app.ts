2350ec179011ddf6b8da0da3ad9bacc2e17b1720
mouadlouhichi <mouadlouhichi@gmail.com>
2023-10-04 03:37:03 +0100

    🐛 fix: NEXT_PUBLIC_APP_URL

--- src/app.ts
+++ src/app.ts
@@ -42,7 +42,7 @@ export const REPOSITORY_OWNER = "mouadlouhichi";
 export const REPOSITORY_NAME = "mindrested-app";
 export const REPOSITORY_URL = `https://github.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}`;
 export const baseUrl =
-  process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+  process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
 
 // ========================================================
 
