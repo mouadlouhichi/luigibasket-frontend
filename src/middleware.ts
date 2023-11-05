03add07b1c2227a2963937fb0f1d1b20df3f5dac
mouadlouhichi <mouadlouhichi@gmail.com>
2023-11-05 15:35:58 +0100

    📝 fix: middleware load

--- src/middleware.ts
+++ src/middleware.ts
@@ -60,21 +60,6 @@ export async function middleware(req: NextRequest) {
     }
   }
 
-  //TODO: handle routes based on role and survey
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-  const role = user?.user_metadata?.role;
-  const hasSurvey = user?.user_metadata?.hasSurvey;
-  if (doesPathMatchPages(req, surveyPages) && hasSurvey) {
-    return redirect(req, defaultUserPage);
-  }
-  // controle access to home page
-  if (doesPathMatchPages(req, adminPages) && role !== ADMIN_ROLE) {
-    return redirect(req, defaultUserPage);
-  }
-
   if (req.nextUrl.pathname.startsWith("/api")) {
     if (true) return NextResponse.next();
   }
@@ -82,84 +67,6 @@ export async function middleware(req: NextRequest) {
   return intlMiddleware(req);
 }
 
-/* function doesPathMatchPages(req: NextRequest, pages: string[]) {
-  return RegExp(
-    `^(/(${languages.join("|")}))?(${pages.join("|")  })/?$`,
-    "i",
-  ).test(req.nextUrl.pathname);
-}
-
-// TODO : list all protected routes
-const protectedPages = ["/home", "/account"];
-
-const defaultPublicPage = "";
-const defaultBlockedPage = "/";
-const defaultUserPage = "/home";
-const blockedPages = ["/account"];
-const surveyPages = ["/survey", "/survey/next"];
-
-const authPages = ["/login", "/signup"];
-const adminPages = ["/admin"];
-const publicPages = [""];
-
-function redirect(req: NextRequest, redirectURL: string) {
-  return NextResponse.redirect(
-    new URL(redirectURL, req.nextUrl.origin).toString(),
-  );
-}
-
-export default withAuth(
-  function onSuccess(req) {
-    const token = req.nextauth.token;
-
-    if (req.nextUrl.pathname.startsWith("/api")) {
-      if (true) return NextResponse.next();
-    }
-
-     if (!token) {
-      if (
-        !doesPathMatchPages(req, authPages) &&
-        !doesPathMatchPages(req, publicPages)
-      ) {
-        return null;
-      }
-      return intlMiddleware(req);
-    }
-
-    if (!token) {
-      if (doesPathMatchPages(req, protectedPages)) {
-        return redirect(req, defaultPublicPage);
-      }
-      return intlMiddleware(req);
-    }
-
-    // todo: make it more stable
-    // if (
-    //   doesPathMatchPages(req, authPages) ||
-    //   (doesPathMatchPages(req, blockedPages) && !token.isBlocked) ||
-    //   (doesPathMatchPages(req, adminPages) && !token.isAdmin)
-    // ) {
-    //   return redirect(req, defaultPublicPage);
-    // }
-
-    // controle access to survey pages
-    if (doesPathMatchPages(req, surveyPages) && token.hasSurvey) {
-      return redirect(req, defaultUserPage);
-    }
-    // controle access to home page
-    if (doesPathMatchPages(req, publicPages) && !token.isAdmin) {
-      return redirect(req, defaultUserPage);
-    }
-
-    return intlMiddleware(req);
-  },
-  {
-    callbacks: {
-      authorized: () => true,
-    },
-  },
-);
- */
 export const config = {
   // Skip all paths that should not be internationalized. This example skips the
   // folders "api", "_next" and all files with an extension (e.g. favicon.ico)
